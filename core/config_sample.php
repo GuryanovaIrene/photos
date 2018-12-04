1ccd72e73ecdb99715b001a8fdbb542934d72429
GuryanovaIrene <44869308+GuryanovaIrene@users.noreply.github.com>
2018-12-04 16:03:57 +0300

    Update config_sample.php

--- core/config_sample.php
+++ core/config_sample.php
@@ -1,15 +1,21 @@
 <?php
+
 const DSN = "mysql:host=127.0.0.1;dbname=photos;charset=utf8";
+
 const DOUBLE_USER = 'Пользователь с таким e-mail уже есть в системе';
 const WRONG_USER = 'Неверная пара пользователь/пароль';
 const WRONG_PASSWORD = 'Неверный пароль';
-const SUCCESS_GENERATE_USER = 'Пользователь сгенерирован успешно';
-const TRY_ENTER_AGAIN = 'Попытаться войти снова';
+
 const BAN_FILE_TYPE = 'Запрещённый тип файла';
 const LARGE_FILE = 'Слишком большой размер файла';
 const FILE_IS_NOT_LOADED = 'Файл не загрузился';
 const SUCCESS = 'Загрузка прошла успешно';
-const ERR_USER_TITLE= 'Были обнаружены следующие ошибки: ';
+const ERR_USER_TITLE= 'При вводе обнаружены следующие ошибки: ';
 const ERR_FILE_TITLE = 'При загрузке возникли ошибки:';
+const SUCCESS_GENERATE_USER = 'Пользователь сгенерирован успешно';
+const TRY_ENTER_AGAIN = 'Попытаться войти снова';
 const MODE = 'dev';
-const PATH = 'images/';
+const PATH = '../images/';
+
+const SORT_TYPE_ASC = 'по возрастанию';
+const SORT_TYPE_DESC = 'по убыванию';
